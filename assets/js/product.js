5b091ff2188ea1be780e6e3dee19b720f1279b1c
Pham Huynh Duc <111016470+Duckun1909@users.noreply.github.com>
2022-08-22 16:20:26 +0700

    Delete assets directory

--- assets/js/product.js
+++
@@ -1,6 +0,0 @@
-$('.product-body__filter-title .hover-back-button').click(function(){
-    $('.product-tool').css('transform', 'translateX(0px)');
-})
-$('.product-tool__close').click(function(){
-    $('.product-tool').css('transform', 'translateX(100%)');
-})
