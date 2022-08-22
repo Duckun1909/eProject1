e6602c36348cd95773c7d9b29df54e7cd42f6b30
Pham Huynh Duc <111016470+Duckun1909@users.noreply.github.com>
2022-08-22 16:29:44 +0700

    HuynhDuc

---
+++ assets/js/product.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.product-body__filter-title .hover-back-button').click(function(){
+        $('.product-tool').css('transform', 'translateX(0px)');
+    })
+    $('.product-tool__close').click(function(){
+        $('.product-tool').css('transform', 'translateX(100%)');
+    })
+
+    
+    $('.headerAbout').css({'background-color': '#fff', 'position': 'unset', 'padding': '20px 0px'});
+    $(window).scroll(function(){
+    var scroll_top = $(this).scrollTop();
+        if(scroll_top > 100){
+            $('.headerAbout').css({'position':'fixed', 'padding': 'unset'});
+        }else{
+            $('.headerAbout').css({'background-color': '#fff', 'position': 'unset', 'padding': '20px 0px'});
+        }
+    })
+})
