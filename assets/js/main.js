552ed3e3be45665749ae723904a93534da4eb138
Pham Huynh Duc <111016470+Duckun1909@users.noreply.github.com>
2022-08-22 14:51:55 +0700

    HuynhDuc

---
+++ assets/js/main.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+    $.ajax({
+        url: '../home.html',
+        type: 'GET',
+        dataType: 'html',
+        success: function(data) {
+            $('.body').html(data)
+        }
+    })
+
+    var checkAnimation = function(nameQuery, nameSiblings, first_Animation, late_Animation) {
+        let checkZoom = 1;
+        $(nameQuery).click(function(){
+            if(checkZoom==1){
+                $(this).siblings(nameSiblings).addClass(first_Animation).removeClass(late_Animation);
+                checkZoom=0;
+            }else{
+                $(this).siblings(nameSiblings).addClass(late_Animation).removeClass(first_Animation);
+                checkZoom=1;
+            }
+        })
+    }
+    
+    checkAnimation('.widget-group-search > i', 'div', 'search_bar--zoomIn', 'search_bar--zoomOut');
+    checkAnimation('.widget-group-user > i', 'div', 'user_own--flipInX', 'search_bar--zoomOut');
+    
+    
+    // Subnav Angle Icon Animation
+    var checkSubnavIcon = function(nameQuery, nameChildren, firstClass, lateClass) {
+        $(nameQuery).mouseover(function(){
+            $(this).children(nameChildren).removeClass(firstClass).addClass(lateClass);
+        })
+        $(nameQuery).mouseout(function(){
+            $(this).children(nameChildren).removeClass(lateClass).addClass(firstClass);
+        })
+    }
+    
+    checkSubnavIcon('.header__navbar-product, .header__navbar-service, .header__navbar-library', 'i.fa-solid', 'fa-angle-down', 'fa-angle-up');
+    checkSubnavIcon('.subnav-support, .subnav-transfer, .subnav-image, .subnav-video', 'i.fa-solid', 'fa-angle-right', 'fa-angle-left')
+    
+    // Header Nav-link Active
+    $('.header__navbar .nav-item').click(function(){
+        $(this).addClass('nav-link--active').siblings().removeClass('nav-link--active');
+    })
+    
+    
+    $(window).scroll(function(){
+        var scroll_top = $(window).scrollTop();
+        var slider_height = $('.slider').height();
+        if(scroll_top > slider_height - 56){
+            $('.header').css({'background-color': '#fff', 'box-shadow':'rgb(163 163 163) -1px 0px 5px'});
+        }else if(scroll_top < slider_height - 56){
+            $('.header').css({'background-color': 'transparent', 'box-shadow':'none'});
+        }
+
+        if(scroll_top > $('body').height()/3){
+            console.log(scroll_top)
+            $('.btn-gotofirst').addClass('btn-gotofirst--animation');
+        }else{
+            $('.btn-gotofirst').removeClass('btn-gotofirst--animation');
+        }
+    })
+    
+    
+    // About Us Ajax
+    $('.nav-item-home').click((e)=>{
+        e.preventDefault();
+        $.ajax({
+            url: '../home.html',
+            type: 'GET',
+            dataType: 'html',
+            success: function(data) {
+                $('.body').html(data)
+            }
+        })
+    })
+
+    $('.nav-item-aboutUs').click((e)=>{
+        e.preventDefault();
+        $.ajax({
+            url: '../aboutUs.html',
+            type: 'GET',
+            dataType: 'html',
+            success: function(data){
+                window.scrollTo(0, 0);
+                $('.body').html(data);
+                $(window).scroll(function(){
+                    var scroll_top = $(this).scrollTop();
+                    if(scroll_top > 100){
+                        $('.header').css({'position':'fixed', 'padding': 'unset'});
+                    }else{
+                        $('.header').css({'background-color': '#fff', 'position': 'unset', 'padding': '20px 0px'});
+                    }
+                })
+            }
+        })
+        $('.header').css({'background-color': '#fff', 'position': 'unset', 'padding': '20px 0px'});
+    })
+
+})
---
+++ assets/js/product.js
@@ -0,0 +1,6 @@
+$('.product-body__filter-title .hover-back-button').click(function(){
+    $('.product-tool').css('transform', 'translateX(0px)');
+})
+$('.product-tool__close').click(function(){
+    $('.product-tool').css('transform', 'translateX(100%)');
+})
